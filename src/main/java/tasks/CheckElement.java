964de688521d9db94965db5546bec6b5504b267d
giovi-hub <71414523+giovi-hub@users.noreply.github.com>
2020-09-16 20:34:29 -0400

    Update CheckElement.java

--- src/main/java/tasks/CheckElement.java
+++ src/main/java/tasks/CheckElement.java
@@ -2,8 +2,15 @@ package tasks;
 
 import org.openqa.selenium.WebDriver;
 
-public class CheckElement {
-    public static void check(WebDriver driver){
-        //TODO ADD Logic
+public void CheckboxLink(WebDriver driver){
+        Click.on(driver, ListElementsUI.checkboxOption);
+    }
+
+    public static void ClickChecbox1(WebDriver driver){
+        Click.on(driver, CheckboxesUI.checkbox1);
+
+    }
+    public static boolean isSelectedCheck1(WebDriver driver){
+         return Check.SelectCheck(driver,CheckboxesUI.checkbox1);
     }
 }
