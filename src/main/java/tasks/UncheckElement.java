f14340673a493569c70c4bbd428fdc4c5b806235
giovi-hub <71414523+giovi-hub@users.noreply.github.com>
2020-09-16 20:34:58 -0400

    Update UncheckElement.java

--- src/main/java/tasks/UncheckElement.java
+++ src/main/java/tasks/UncheckElement.java
@@ -3,7 +3,10 @@ package tasks;
 import org.openqa.selenium.WebDriver;
 
 public class UncheckElement {
-    public static void uncheck(WebDriver driver){
-        //TODO ADD Logic
+    public static void ClickChecbox2(WebDriver driver){
+        Click.on(driver, CheckboxesUI.checkbox2);
+    }
+    public static boolean isUnSelectedCheck2(WebDriver driver){
+        return Check.SelectCheck(driver,CheckboxesUI.checkbox2);
     }
 }
